cbe062e969965a4fe24aa1d710c4b0ba7eddf138
Arun Natarajan <arun.n.ece@gmail.com>
2010-10-07 19:38:29 -0400

    added chardev.h content to rta_km.c

--- rta_km.c
+++ rta_km.c
@@ -16,8 +16,66 @@
 #include <linux/delay.h>
 #include <linux/fs.h>
 #include <asm/uaccess.h>	/* for get_user and put_user */
+#include <linux/ioctl.h>
 
-#include "chardev.h"
+//#include "chardev.h"
+
+
+
+/* 
+ * The major device number. We can't rely on dynamic 
+ * registration any more, because ioctls need to know 
+ * it. 
+ */
+#define MAJOR_NUM 100
+
+/* 
+ * Set the message of the device driver 
+ */
+#define IOCTL_SET_MSG _IOR(MAJOR_NUM, 0, char *)
+/*
+ * _IOR means that we're creating an ioctl command 
+ * number for passing information from a user process
+ * to the kernel module. 
+ *
+ * The first arguments, MAJOR_NUM, is the major device 
+ * number we're using.
+ *
+ * The second argument is the number of the command 
+ * (there could be several with different meanings).
+ *
+ * The third argument is the type we want to get from 
+ * the process to the kernel.
+ */
+
+/* 
+ * Get the message of the device driver 
+ */
+#define IOCTL_GET_MSG _IOR(MAJOR_NUM, 1, char *)
+/* 
+ * This IOCTL is used for output, to get the message 
+ * of the device driver. However, we still need the 
+ * buffer to place the message in to be input, 
+ * as it is allocated by the process.
+ */
+
+/* 
+ * Get the n'th byte of the message 
+ */
+#define IOCTL_GET_NTH_BYTE _IOWR(MAJOR_NUM, 2, int)
+/* 
+ * The IOCTL is used for both input and output. It 
+ * receives from the user a number, n, and returns 
+ * Message[n]. 
+ */
+
+/* 
+ * The name of the device file 
+ */
+#define DEVICE_FILE_NAME "char_dev"
+
+
+
 #define SUCCESS 0
 #define DEVICE_NAME "char_dev"
 #define BUF_LEN 80
