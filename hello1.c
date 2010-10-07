3b2686318d56fab75f5e7985466fd5859d073906
Arun Natarajan <arun.n.ece@gmail.com>
2010-10-07 12:01:17 -0400

    skb xmit works

--- hello1.c
+++ hello1.c
@@ -11,7 +11,11 @@
 #include <net/sock.h>
 #include <net/icmp.h>
 #include <linux/errno.h>
-
+#include <linux/skbuff.h>
+#include <linux/netfilter.h>
+#include <linux/netfilter_ipv4.h>
+#include <linux/ip.h>
+#include <linux/udp.h>
 #include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/init.h>
@@ -31,7 +35,7 @@
 #define CONNECT_PORT 16767
 #define MODULE_NAME "ksocket"
 //#define INADDR_SEND ((unsigned long int)0x7f000001) /* 127.0.0.1 */
-#define INADDR_SEND ((unsigned long int)0x980E5C01) /* 127.0.0.1 */
+#define INADDR_SEND ((unsigned long int)0xB010F680) /* 127.0.0.1 */
 //#define INADDR_SEND INADDR_LOOPBACK
 
 
@@ -40,7 +44,12 @@
 int ksocket_send(struct socket *sock, struct sockaddr_in *addr, unsigned char *buf, int len);
 
 
-
+struct sk_buff *sock_buff;
+struct iphdr *ip_header;
+struct udphdr *udp_header;
+static struct nf_hook_ops nfho;
+struct sk_buff *udp_skb;
+int times=0;
 
 
 
@@ -98,162 +107,6 @@ return 0;
 
 } 
 
-int send_udp_packet_old(void)  {
-mm_segment_t oldfs; 
-int size = 0; 
-int error = 0; 
-char *warp_dev="eth0"; 
-char dev[20]; 
-char*buf="This is a message from kernel";
-struct iovec iov; 
-struct sockaddr sa; 
-struct msghdr msg; 
-
-struct socket *sock; 
-//int source_port=16767;
-
-if ((error=sock_create(PF_INET, SOCK_DGRAM, 0, &sock))<0) { 
-printk(KERN_WARNING "Could not create a PF_PACKET SOCK_RAW Socket\n"); 
-return (-1); 
-} 
-
-memset(&sa,0,sizeof(sa)); 
-strcpy(dev,warp_dev); 
-strncpy(sa.sa_data,dev,sizeof(sa.sa_data)); 
-
-error = sock->ops->bind(sock,&sa,sizeof(sa)); 
-if (error<0) 
-{ 
-printk(KERN_WARNING "Error binding socket"); 
-return -1; 
-} 
- //the bind operations
- 
-    /*   addr_in->sin_family=PF_INET;
-       addr_in->sin_port = htons(source_port);
-       addr_in->sin_addr.s_addr = source_address;
-       memset(&(addr_in->sin_zero), '\0', 8);
- 
-       knetlog->addr_in = addr_in;
-       retval = sock->ops->bind(sock, (struct sockaddr *) knetlog->addr_in,
-		sizeof(struct sockaddr));*/
- 
-//to enable broadcast:
-   /*    lock_sock(sock->sk);
-       sock->sk->broadcast = 1;
-       release_sock(sock->sk);
- */
-iov.iov_base = (char *)buf; 
-iov.iov_len = (__kernel_size_t) strlen(buf); 
-
-msg.msg_iov = &iov; 
-msg.msg_iovlen = 1; 
-msg.msg_control = NULL; 
-msg.msg_controllen = 0; 
-msg.msg_name = NULL; 
-msg.msg_namelen = 0; 
-msg.msg_flags = 0; 
-
- 
-// now, your UDP packet could be sent by:
-       oldfs = get_fs(); 
-       set_fs(KERNEL_DS);   /* Required to change addr_limit */
-       error = sock_sendmsg(sock, &msg, iov.iov_len);
-       set_fs(oldfs);
-
-if (error < 0) 
-printk(KERN_WARNING "sock_sendmsg error: %d\n",size); 
-
-return 0;
-}
-
-
-int send_udp_packet_2nd(void)
-{
-	u32 destination;
-	u16 port = 16767;
-	u16 id =12122;
-	__be16 outbuf = htons(id);
-	struct socket *sock;
-	struct sockaddr sa; 
-	struct msghdr msg;
-	char dev[20]; 
-        struct iovec iov;
-	struct sockaddr_in whereto;
-	char *warp_dev="eth0"; 
-	int size;	
-	char *address="152.14.92.55";
-	char *buf="This is a message from kernel";
-//	int len = 20;
-	int len = sizeof(u16);
-	int ret;
-	destination = ((unsigned long int)0x7f000001);
-	printk("destination: %u\n", destination);
-
-
-memset(&sa,0,sizeof(sa)); 
-strcpy(dev,warp_dev); 
-strncpy(sa.sa_data,dev,sizeof(sa.sa_data)); 
-	
-	ret=sock_create(AF_INET, SOCK_DGRAM, IPPROTO_UDP, &sock);
-	
-	
-        if(ret < 0) {
-	printk("socket create error :\n");
-	 return -1;			
-	}
-	
-	/* fillout sockaddr_in-structure whereto */
-
-	port = htons(port);
-
-	memset(&whereto, 0, sizeof(whereto));
-	whereto.sin_family = AF_INET;
-	whereto.sin_addr.s_addr = htonl(destination);
-	whereto.sin_port =htons(port);
-
-//ret = sock->ops->bind(sock,&sa,sizeof(sa)); 
-/*if (ret<0) 
-{ 
-printk(KERN_WARNING "Error binding socket %d" , ret); 
-return -1; 
-} */
-
-	/* "connect" */
-	if( (sock->ops->connect(sock, (struct sockaddr *)&whereto,
-				sizeof(struct sockaddr), 0) )< 0)
-		return -1;
-	printk("connected:\n");
-        iov.iov_base = buf;
-        iov.iov_len = len;
-	
-        msg.msg_flags = 0;
-        msg.msg_name = &whereto;
-        msg.msg_namelen  = sizeof(struct sockaddr_in);
-        msg.msg_control = NULL;
-        msg.msg_controllen = 0;
-        msg.msg_iov = &iov;
-        msg.msg_iovlen = 1;
-        msg.msg_control = NULL;
-
-	
-        /* fire! */
-	len = iov.iov_len;
-
-		size = sock_sendmsg(sock, &msg, len);
-		printk("sock_sendmsg : %d\n", size);		
-		if (size < 0){
-			printk("sock_sendmsg error: %d\n", size);			
-		}
-
-	
-	sock_release(sock);
-	
-
-	return 0;
-}
-
-
 
 long long timeval_diff(struct timeval *difference, struct timeval *end_time, struct timeval *start_time)
 {
@@ -279,6 +132,39 @@ long long timeval_diff(struct timeval *difference, struct timeval *end_time, str
                    difference->tv_usec;
 
 } /* timeval_diff() */
+
+
+static unsigned int hook_func(unsigned int hooknum,
+	       		struct sk_buff *skb,
+				const struct net_device *in,
+				const struct net_device *out,
+				int (*okfn)(struct sk_buff *))
+{
+	if(times>=1) {
+		return NF_ACCEPT;
+	}
+	sock_buff = skb; 
+
+	if (!sock_buff) {
+		return NF_ACCEPT;
+	} else {
+		ip_header = (struct iphdr *)skb_network_header(sock_buff);
+		if (!ip_header) {
+			return NF_ACCEPT;
+		} else {
+			if(ip_header->protocol == 17) {
+				printk(KERN_INFO "start saw a udp packet %u\n", udp_skb);
+				udp_skb = skb_clone (skb, GFP_ATOMIC);
+				printk(KERN_INFO "saw a udp packet %u\n", udp_skb);
+				times ++;
+				return NF_ACCEPT;
+			}
+			else {
+				return NF_ACCEPT;
+			}
+		}
+	}
+}
 
 
 
@@ -362,7 +248,33 @@ int ksocket_send(struct socket *sock, struct sockaddr_in *addr, unsigned char *b
         return size;
 }
 
-int send_skb_packet(void) {
+int send_skb_packet(struct sk_buff *skb) {
+	printk("In send skb : skb is %u\n",skb);
+	
+	
+	struct net_device * eth4 = dev_get_by_name(&init_net, "eth4" ) ;
+	struct net_device * eth5 = dev_get_by_name(&init_net, "eth5" ) ;
+
+	
+	printk("pkt type :%d %s\n", skb->pkt_type, skb->dev->name);
+	skb->pkt_type = PACKET_OUTGOING; 
+	printk("pkt type :%d \n", skb->pkt_type);
+	
+	if (eth4 == skb->dev)
+	{
+		skb->dev = eth5;
+	} else if (eth5 == skb->dev)
+	{
+		skb->dev = eth4;
+	} else {
+			skb->dev = eth4;
+	}
+	
+	
+	
+	dev_queue_xmit(skb); 
+	
+	return 1;
 /*	struct net_device * eth0 = dev_get_by_name(&init_net, "eth0" ) ;
 	struct sk_buff *skb;
 	
@@ -404,12 +316,19 @@ int init_module(void)
     //   printk(KERN_INFO "calling arp send\n");
     // send_arp_request();
 
-    printk(KERN_INFO " calling udp\n");
+   
+	nfho.hook     = hook_func;
+   	nfho.hooknum  = 1;
+	nfho.pf       = PF_INET;
+	nfho.priority = NF_IP_PRI_FIRST;
+	nf_register_hook(&nfho);
+
+	printk("Called hook fn\n");
+	
+	printk(KERN_INFO " calling udp\n");
 	send_udp_packet();
-    return 0;
-    
-    printk(KERN_INFO " calling skb send\n");
-	send_skb_packet();
+
+	
     return 0;
     
     
@@ -417,7 +336,18 @@ int init_module(void)
 
 void cleanup_module(void)
 {
-        printk(KERN_INFO "cleanup_module() called\n");
+	
+	printk(KERN_INFO"Unloading the module. times is %d..\n", times);
+	
+	if(times>0) {
+		printk(KERN_INFO " calling skb send\n");
+		send_skb_packet(udp_skb);
+	}
+	
+	kfree_skb(udp_skb);
+	nf_unregister_hook(&nfho);
+	
+    printk(KERN_INFO "cleanup_module() called\n");
 }
 
 
